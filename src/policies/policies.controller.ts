4716d2a4114ed4e0863dc8512fe27d259a62fac6
ph8l <jjackson@itscovered.com>
2022-07-20 18:38:16 -0700

    added POST

--- src/policies/policies.controller.ts
+++ src/policies/policies.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { Request } from 'express';
 //url.com/policies
 @Controller('policies')
 export class PoliciesController {
-    
+//return handles all HTTP fun. Status etc.
+    //status 201
+    @Post()
+    addPolicy(): string {
+        return 'policy added'
+    }
+    //status 200
     @Get()
     findPolicy(): string {
-        return 'this action returns all cats'
+        return 'this is your policy'
     }
     
 }
