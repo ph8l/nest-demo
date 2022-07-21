80699e19aceb8492816a4909f4dade3546c7a0ca
ph8l <jjackson@itscovered.com>
2022-07-20 18:14:45 -0700

    policies controller created for GET

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { PoliciesController } from './policies/policies.controller';
+//Root
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, PoliciesController],
   providers: [AppService],
 })
 export class AppModule {}
